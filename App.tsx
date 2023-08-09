b10d95db6b96e05649639db1da837f4b4d23236b
jaocordeiro <joaocordeiroalmeida01@gmail.com>
2023-08-08 22:56:55 -0300

    ajusted component

--- App.tsx
+++ App.tsx
@@ -1,11 +1,21 @@
+import { StatusBar } from 'react-native';
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
 import { ThemeProvider } from 'styled-components';
-import theme from '@theme/index';
 import { Groups } from '@screens/Groups';
+import theme from '@theme/index';
+import { Loading } from '@components/Loading';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
+
   return (
     <ThemeProvider theme={theme}>
-      <Groups />
+      <StatusBar
+        barStyle='light-content'
+        backgroundColor='transparent'
+        translucent
+      />
+      {fontsLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
   );
 }
--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,9 +1,16 @@
-import { Container, Title } from './styles'
+import { Container } from './styles'
+import { Header } from '@components/Header';
+import { Highlight } from '@components/Highlight';
+
 
 export function Groups() {
   return (
     <Container>
-      <Title>Groups!</Title>
+      <Header />
+
+      <Highlight
+        title='Turmas'
+        subtitle='Jogue com sua turma' />
     </Container>
   );
 }
