4f71e4387d6be932f053ba8a89b02a6fe7297c5c
jaocordeiro <joaocordeiroalmeida01@gmail.com>
2023-08-09 22:50:57 -0300

    creat home page app

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,16 +1,37 @@
+import { useState } from 'react';
+import { FlatList } from 'react-native'
 import { Container } from './styles'
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
-
+import { GroupCard } from '@components/GroupCard';
+import { ListEmpty } from '@components/ListEmpty';
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>([])
+
+
   return (
     <Container>
       <Header />
 
       <Highlight
         title='Turmas'
-        subtitle='Jogue com sua turma' />
+        subtitle='Jogue com sua turma'
+      />
+
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => String(item)}
+        renderItem={({ item }) => (
+          <GroupCard
+            title={item}
+          />
+        )}
+        contentContainerStyle={groups.length === 0 && { flex: 1, top: -50 }}
+        ListEmptyComponent={() => (
+          <ListEmpty message='Cadastre a Primeira turma!' />
+        )}
+      />
     </Container>
   );
 }
