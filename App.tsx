8d5fb0251abec42fe1be6999816eefa6001a787e
jaocordeiro <joaocordeiroalmeida01@gmail.com>
2023-08-15 22:11:11 -0300

    ajusted

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import { StatusBar } from 'react-native';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
 import { ThemeProvider } from 'styled-components';
-import { NewGroup } from '@screens/NewGroup';
+import { Players } from '@screens/Players';
 import theme from '@theme/index';
 import { Loading } from '@components/Loading';
 
@@ -15,7 +15,7 @@ export default function App() {
         backgroundColor='transparent'
         translucent
       />
-      {fontsLoaded ? <NewGroup /> : <Loading />}
+      {fontsLoaded ? <Players /> : <Loading />}
     </ThemeProvider>
   );
 }
--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -2,6 +2,7 @@ import { Header } from '@components/Header';
 import { Container, Content, Icon } from './styles';
 import { Highlight } from '@components/Highlight';
 import { Button } from '@components/Button';
+import { Input } from '@components/Input';
 
 export function NewGroup() {
   return (
@@ -12,6 +13,8 @@ export function NewGroup() {
         <Icon />
 
         <Highlight title='Nova turma' subtitle='Crie uma nova turma para add pessoas' />
+
+        <Input placeholder='Nome da turma' />
 
         <Button title='Criar' />
       </Content>
