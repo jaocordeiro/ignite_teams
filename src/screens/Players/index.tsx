0cea9e83f881b0384326024f7310a837d54ac70c
jaocordeiro <joaocordeiroalmeida01@gmail.com>
2023-08-16 21:53:06 -0300

    ajusted screen Players

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,8 +1,21 @@
+import { useState } from 'react';
+import { FlatList } from 'react-native';
+import {
+  Container,
+  Form,
+  HeaderList,
+  NumberOfPlayers
+} from './styles';
 import { Header } from '@components/Header';
-import { Container } from './styles';
+import { Filter } from '@components/Filter'
 import { Highlight } from '@components/Highlight';
+import { ButtonIcon } from '@components/ButtonIcon';
+import { Input } from '@components/Input';
 
 export function Players() {
+  const [team, setTeam] = useState('')
+  const [players, setPlayers] = useState([])
+
   return (
     <Container>
       <Header
@@ -13,6 +26,33 @@ export function Players() {
         title='Nome da turma'
         subtitle='Adicione a galera e separe os times'
       />
+
+      <Form>
+        <Input
+          placeholder='Nome da pessoa'
+          autoCorrect={false}
+        />
+        <ButtonIcon icon='add' type='PRIMARY' />
+      </Form>
+
+      <HeaderList>
+        <FlatList
+          data={['time a', 'time b']}
+          keyExtractor={item => item}
+          renderItem={({ item }) => (
+            <Filter
+              title={item}
+              isActive={item === team}
+              onPress={() => setTeam(item)}
+            />
+          )}
+          horizontal
+        />
+        <NumberOfPlayers>
+          {players.length}
+        </NumberOfPlayers>
+      </HeaderList>
+
     </Container>
   )
 }
