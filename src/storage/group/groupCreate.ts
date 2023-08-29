ffa8daf8654d6524fc1ccbb26e07c7f9c4580437
jaocordeiro <joaocordeiroalmeida01@gmail.com>
2023-08-28 22:13:51 -0300

    save infos users and groups in AsyncStorage

--- src/storage/group/groupCreate.ts
+++ src/storage/group/groupCreate.ts
@@ -1,9 +1,18 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { GROUP_COLLECTION } from '@storage/storageConfig';
+import { groupsGetAll } from "./groupsGetAll";
+import { AppError } from "@utils/AppError";
 
 export async function groupCreate(newGroup: string) {
   try {
-    await AsyncStorage.setItem(GROUP_COLLECTION, newGroup)
+    const storedGroups = await groupsGetAll();
+    const groupAlreadyExists = storedGroups.includes(newGroup);
+
+    if (groupAlreadyExists) {
+      throw new AppError('Já existe um grupo com este nome.')
+    }
+    const storage = JSON.stringify([...storedGroups, newGroup])
+    await AsyncStorage.setItem(GROUP_COLLECTION, storage)
   } catch (error) {
     throw error;
   }
